b1309f0ac512bad2d3cce720ae6c69f9a9e37e03
teljaoui <teljaoui2004@gmail.com>
2026-04-07 22:20:31 +0100

    Finalisation du Architecture des formulaire

--- administrator/urls.py
+++ administrator/urls.py
@@ -3,10 +3,18 @@ from . import views
 
 urlpatterns = [
     path('', views.admin_dashboard, name='admin_dashboard'),
+
+    # USERS
+    path('utilisateurs/', views.user_index, name='user_index'),
+    path('utilisateurs/ajouter/', views.user_add, name='user_add'),
+    path('utilisateurs/modifier/<int:id>/', views.user_update, name='user_update'),
+
+    # OPTIMISATION
     path('optimisation/index/', views.optimisation_index, name='liste_optimisation'),
     path('optimisation/ajouter/', views.optimisation_add, name='ajouter_optimisation'),
-    path('optimisation/modifer/', views.optimisation_update, name='modifer_optimisation'),
-    path('optimisation/zone/index', views.optimisation_zone, name='zone_optimisation'),
-    path('optimisation/zone/detail', views.optimisation_zone_detail, name='zone_detail_optimisation'),
+    path('optimisation/modifier/<int:id>/', views.optimisation_update, name='modifier_optimisation'),
 
+    # ZONE
+    path('optimisation/zone/index/', views.optimisation_zone, name='zone_optimisation'),
+    path('optimisation/zone/detail/<int:id>/', views.optimisation_zone_detail, name='zone_detail_optimisation'),
 ]
--- administrator/views.py
+++ administrator/views.py
@@ -1,6 +1,7 @@
 # administrator/views.py
 from accounts.decorators import admin_required
 from django.shortcuts import render
+from accounts.models import User
 
 @admin_required
 def admin_dashboard(request):
@@ -25,5 +26,25 @@ def optimisation_zone(request):
 @admin_required
 def optimisation_zone_detail(request):
       return render(request, 'administrator/pages/optimisation/zone.html')
+
+@admin_required
+def user_index(request):
+    current_user_id = request.session.get('user_id')
+
+    users = User.objects.exclude(id=current_user_id)
+
+    return render(request, 'administrator/pages/users/index.html', {
+        'users': users
+    })
+
+@admin_required
+def user_add(request):
+      return render(request, 'administrator/pages/users/ajouter.html')
+
+@admin_required
+def user_update(request):
+      return render(request, 'administrator/pages/users/modifier.html')
+
+
 
 
